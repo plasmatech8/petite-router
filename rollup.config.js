0df15618c3be7b84f3ed9cb5fc3026e97ab020a6
Mark Connelly <com0004@gmail.com>
2024-06-02 02:03:31 +1000

    Add source maps

--- rollup.config.js
+++ rollup.config.js
@@ -8,28 +8,32 @@ export default {
       file: 'dist/petite-router.js',
       format: 'commonjs',
       name: 'PetiteRouter',
-      plugins: [terser()]
+      plugins: [terser()],
+      sourcemap: true
     },
     // ES Module
     {
       file: 'dist/petite-router.es.js',
       format: 'es',
       name: 'PetiteRouter',
-      plugins: [terser()]
+      plugins: [terser()],
+      sourcemap: true
     },
     // Universal Module Definition
     {
       file: 'dist/petite-router.umd.js',
       format: 'umd',
       name: 'PetiteRouter',
-      plugins: [terser()]
+      plugins: [terser()],
+      sourcemap: true
     },
     // Add to example code
     {
       file: 'example/petite-router.es.js',
       format: 'es',
       name: 'PetiteRouter',
-      plugins: [terser()]
+      plugins: [terser()],
+      sourcemap: true
     }
   ],
   plugins: []
