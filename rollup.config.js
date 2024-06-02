76d939d25ec0c7170bf6b463656adaf2cfb05520
Mark Connelly <com0004@gmail.com>
2024-06-02 22:12:41 +1000

    Add iife version

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,63 @@
 import terser from '@rollup/plugin-terser';
 
-export default {
-  input: 'src/petite-router.js',
-  output: [
-    // CommonJS
-    {
-      file: 'dist/petite-router.js',
-      format: 'commonjs',
-      name: 'PetiteRouter',
-      plugins: [terser()],
-      sourcemap: true
-    },
-    // ES Module
-    {
-      file: 'dist/petite-router.es.js',
-      format: 'es',
-      name: 'PetiteRouter',
-      plugins: [terser()],
-      sourcemap: true
-    },
-    // Universal Module Definition
-    {
-      file: 'dist/petite-router.umd.js',
-      format: 'umd',
-      name: 'PetiteRouter',
-      plugins: [terser()],
-      sourcemap: true
-    },
-    // Add to example code
-    {
-      file: 'example/petite-router.es.js',
-      format: 'es',
-      name: 'PetiteRouter',
-      plugins: [terser()],
-      sourcemap: true
-    }
-  ],
-  plugins: []
-};
+export default [
+  {
+    input: 'src/petite-router.js',
+    output: [
+      // CommonJS
+      {
+        file: 'dist/petite-router.js',
+        format: 'commonjs',
+        name: 'PetiteRouter',
+        plugins: [terser()],
+        sourcemap: true
+      },
+      // ES Module
+      {
+        file: 'dist/petite-router.es.js',
+        format: 'es',
+        name: 'PetiteRouter',
+        plugins: [terser()],
+        sourcemap: true
+      },
+      // Universal Module Definition
+      {
+        file: 'dist/petite-router.umd.js',
+        format: 'umd',
+        name: 'PetiteRouter',
+        plugins: [terser()],
+        sourcemap: true
+      },
+      // Add to example code
+      {
+        file: 'example/petite-router.es.js',
+        format: 'es',
+        name: 'PetiteRouter',
+        plugins: [terser()],
+        sourcemap: true
+      }
+    ],
+    plugins: []
+  },
+  {
+    input: 'src/iife-script.js',
+    output: [
+      //  Immediately Invoked Function Expression
+      {
+        file: 'dist/petite-router.iife.js',
+        format: 'iife',
+        name: 'PetiteRouter',
+        plugins: [terser()],
+        sourcemap: true
+      },
+      // Add to example code
+      {
+        file: 'example/petite-router.iife.js',
+        format: 'iife',
+        name: 'PetiteRouter',
+        plugins: [terser()],
+        sourcemap: true
+      }
+    ]
+  }
+];
---
+++ src/iife-script.js
@@ -0,0 +1,4 @@
+import { createRouter } from './petite-router';
+
+// @ts-ignore
+window.router = createRouter().mount();
